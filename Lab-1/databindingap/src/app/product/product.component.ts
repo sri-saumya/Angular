23a344df339183d218cadafe3999e6ba5a2e45fb
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-04-06 11:07:10 -0700

    angular

---
+++ Lab-1/databindingap/src/app/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DataserviceService } from '../dataservice.service';
+
+import { IProduct } from './IProduct';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  
+  products : IProduct[] = []
+  constructor(private DataService:DataserviceService) {
+
+   }
+ 
+  ngOnInit(): void {
+    this.products = this.DataService.getProducts();
+    
+  }
+
+  searchtext:string;
+  searchpipe:number;
+
+  
+}
